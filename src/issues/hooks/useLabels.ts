0698bf2b367826659d142df05e1c009b109f4b20
LostoVayne <moises.barrera.dev@gmail.com>
2025-03-16 01:28:09 -0300

    agrega placeholder al customhook

--- src/issues/hooks/useLabels.ts
+++ src/issues/hooks/useLabels.ts
@@ -7,6 +7,32 @@ export const useLabels = () => {
     queryKey: ["labels"],
     queryFn: getLabels,
     staleTime: 1000 * 60 * 60, // 1 hour
+    // placeholderData: [
+    //   {
+    //     id: 196858374,
+    //     node_id: "MDU6TGFiZWwxOTY4NTgzNzQ=",
+    //     url: "https://api.github.com/repos/facebook/react/labels/CLA%20Signed",
+    //     name: "CLA Signed",
+    //     color: "e7e7e7",
+    //     default: false,
+    //   },
+    //   {
+    //     id: 1109410193,
+    //     node_id: "MDU6TGFiZWwxMTA5NDEwMTkz",
+    //     url: "https://api.github.com/repos/facebook/react/labels/Component:%20Hooks",
+    //     name: "Component: Hooks",
+    //     color: "c2f27b",
+    //     default: false,
+    //   },
+    //   {
+    //     id: 760751171,
+    //     node_id: "MDU6TGFiZWw3NjA3NTExNzE=",
+    //     url: "https://api.github.com/repos/facebook/react/labels/Difficulty:%20challenging",
+    //     name: "Difficulty: challenging",
+    //     color: "f2687c",
+    //     default: false,
+    //   },
+    // ] satisfies GithubLabel[],
   });
 
   return {
