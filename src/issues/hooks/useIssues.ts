3dd828b1c8041c1d5fda5c564e785a4f0548f515
LostoVayne <moises.barrera.dev@gmail.com>
2025-04-01 23:01:43 -0300

    refactor(issues): add console log for debugging and integrate useIssues hook in list component
    Add console log to debug issuesQuery in useIssues hook and integrate the hook in the issues list component for better state management.

--- src/issues/hooks/useIssues.ts
+++ src/issues/hooks/useIssues.ts
@@ -2,11 +2,12 @@ import { getIssues } from "@/issues/actions/get-Issues";
 import { useQuery } from "@tanstack/react-query";
 
 export const useIssues = () => {
-
   const issuesQuery = useQuery({
     queryKey: ["issues"],
     queryFn: getIssues,
   });
+
+  console.log(issuesQuery);
 
   return {
     issuesQuery,
--- src/routes/issues/list.tsx
+++ src/routes/issues/list.tsx
@@ -1,6 +1,7 @@
 import { IssueSuspense } from "@/issues/componentes/issue-suspense";
 import { IssueList } from "@/issues/componentes/issues-list";
 import { LabelPicker } from "@/issues/componentes/label-picker";
+import { useIssues } from "@/issues/hooks/useIssues";
 
 import { createFileRoute, ErrorComponent } from "@tanstack/react-router";
 import { Suspense } from "react";
@@ -11,6 +12,8 @@ export const Route = createFileRoute("/issues/list")({
 });
 
 function RouteComponent() {
+  const { issuesQuery } = useIssues();
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 mt-5">
       <div className="col-span-1 sm:col-span-2">
